9ef8eb0ae5a27c8a99ee22400e8170e9a41ba9f3
Cristiana <cristiana.lucaci@student.upt.ro>
2021-04-18 23:27:59 +0300

    Initial commit

---
+++ src/test/java/com/pad/keeponlearning/keeponlearning/KeeponlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pad.keeponlearning.keeponlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeeponlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

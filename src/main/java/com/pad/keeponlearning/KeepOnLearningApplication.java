8904088ed4f70c0bce3e292b8c36b343b9be6c9e
Cristiana <cristiana.lucaci@student.upt.ro>
2021-04-19 23:01:39 +0300

    Developed JPA Entities

---
+++ src/main/java/com/pad/keeponlearning/KeepOnLearningApplication.java
@@ -0,0 +1,13 @@
+package com.pad.keeponlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepOnLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepOnLearningApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pad/keeponlearning/dao/OfferCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.pad.keeponlearning.dao;
+
+import com.pad.keeponlearning.entity.OfferCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "offerCategory", path = "offer-category")
+public interface OfferCategoryRepository extends JpaRepository<OfferCategory, Long> {
+}

bb39e0517ad920272589bab7bd6865143261e1a5
Raluca <carare.raluca27@gmail.com>
2021-04-20 14:13:38 +0300

    Added offer class

---
+++ frontend/keeponlearning/src/app/common/offer.ts
@@ -0,0 +1,13 @@
+export class Offer {
+    professorName: string;
+    name: string;
+    description: string;
+    participants: number;
+    imageUrl: string;
+    active: boolean;
+    placesAvailable: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+}
+
+

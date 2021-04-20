fed27ad46502c526433ede533a73f07fac62c337
Raluca <carare.raluca27@gmail.com>
2021-04-20 13:55:16 +0300

    Added offers list

--- frontend/keeponlearning/src/app/app.module.ts
+++ frontend/keeponlearning/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OfferListComponent } from './components/offer-list/offer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OfferListComponent
   ],
   imports: [
     BrowserModule

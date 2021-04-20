ae3a905b646d29d8df745eb70c4cb854d87786b0
Raluca <carare.raluca27@gmail.com>
2021-04-20 14:43:39 +0300

    Added CrossOrigin - frontend-backend

--- src/main/java/com/pad/keeponlearning/dao/OfferCategoryRepository.java
+++ src/main/java/com/pad/keeponlearning/dao/OfferCategoryRepository.java
@@ -3,7 +3,9 @@ package com.pad.keeponlearning.dao;
 import com.pad.keeponlearning.entity.OfferCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "offerCategory", path = "offer-category")
 public interface OfferCategoryRepository extends JpaRepository<OfferCategory, Long> {
 }

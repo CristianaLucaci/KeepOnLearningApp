4df78d8dc45b7fbfa8310692871312b60e5d428d
iuliadrgn <iuliadragan02@yahoo.com>
2021-04-20 21:44:06 +0300

    left side menu search for offers by category

--- frontend/keeponlearning/src/app/app.module.ts
+++ frontend/keeponlearning/src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
+import {NgModule, OnInit} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { OfferListComponent } from './components/offer-list/offer-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { OfferService } from './services/offer.service'
+import {Routes, RouterModule} from "@angular/router";
 
+const routes: Routes = [
+  {path: 'offer_category/:id', component: OfferListComponent},
+  {path: 'offer_category', component: OfferListComponent},
+  {path: 'offer', component: OfferListComponent},
+  {path: '', redirectTo: '/offer',pathMatch: 'full'},
+  {path: '**', redirectTo: '/offer',pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
     OfferListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
--- frontend/keeponlearning/src/app/components/offer-list/offer-list.component.ts
+++ frontend/keeponlearning/src/app/components/offer-list/offer-list.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Offer } from 'src/app/common/offer';
 import { OfferService } from 'src/app/services/offer.service';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-offer-list',
-  //templateUrl: './offer-list-table.component.html',
-  //templateUrl: './offer-list-table.component.html',
+
   templateUrl: './offer-list-grid.component.html',
 
   styleUrls: ['./offer-list.component.css']
@@ -13,15 +13,32 @@ import { OfferService } from 'src/app/services/offer.service';
 export class OfferListComponent implements OnInit {
 
   offers: Offer[];
+  currentCategoryId: number;
 
-  constructor(private offerService: OfferService) { }
+  constructor(private offerService: OfferService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.listOffers();
+    this.route.paramMap.subscribe(() => {
+      this.listOffers();
+    });
   }
 
   listOffers(){
-    this.offerService.getOfferList().subscribe(
+
+      //check if id parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      //get the id param string. convert string to a number using the "+" symbol
+      this.currentCategoryId= +this.route.snapshot.paramMap.get('id');
+    }
+    else {
+      //no category id available
+      this.currentCategoryId=1;
+    }
+      //get the offers for the given category id
+    this.offerService.getOfferList(this.currentCategoryId).subscribe(
       data => {
         this.offers = data;
       }

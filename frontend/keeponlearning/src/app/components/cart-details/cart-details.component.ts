a34915a3cd4435d97cf4eb6c2662b734552d5317
Raluca <carare.raluca27@gmail.com>
2021-04-28 15:00:54 +0300

    Cart details work

---
+++ frontend/keeponlearning/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit {
+
+  cartItems: CartItem[] = [];
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+  listCartDetails() {
+    //get a hundle to the cart items
+    this.cartItems = this.cartService.cartItems;
+    
+    //subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+
+    //compute cart total quantity
+    this.cartService.computeCartTotals();
+  }
+
+}

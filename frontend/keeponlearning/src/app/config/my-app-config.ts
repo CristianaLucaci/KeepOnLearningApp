96f2a90dddf4030e4c02c485fe181982309dcd23
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2021-05-08 01:12:14 +0300

    Initialized Okta sign in widget

--- frontend/keeponlearning/src/app/config/my-app-config.ts
+++ frontend/keeponlearning/src/app/config/my-app-config.ts
@@ -1,6 +1,6 @@
 export default {
   oidc:{
-    client:'0oapf5e8lVnKgsg825d6',
+    clientId:'0oapf5e8lVnKgsg825d6',
     issuer:'https://dev-32860632.okta.com/oauth2/default',
     redirectUri:'http://localhost:4200/login/callback',
     scopes:['openid','profile','email']

76cf7fe8e54fbffc9b6c2e8f87b5da621c2e526d
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2021-05-08 01:55:08 +0300

    Updated App Module configs to connect routes

--- frontend/keeponlearning/src/app/app.module.ts
+++ frontend/keeponlearning/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { OfferListComponent } from './components/offer-list/offer-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { OfferService } from './services/offer.service'
-import {Routes, RouterModule} from "@angular/router";
+import {Routes, RouterModule, Router} from "@angular/router";
 import { OfferCategoryMenuComponent } from './components/offer-category-menu/offer-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { OfferDetailsComponent } from './components/offer-details/offer-details.component';
@@ -17,10 +17,23 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { LoginComponent } from './components/login/login.component';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
+import{
+  OKTA_CONFIG,
+  OktaAuthModule,
+  OktaCallbackComponent
+} from "@okta/okta-angular";
 
+import myAppConfig from './config/my-app-config';
 
-
+const oktaConfig =Object.assign({
+  onAuthRequired:(injector)=> {
+    const router = injector.get(Router);
+    router.navigate(['/login']);
+  }
+},myAppConfig.oidc);
 const routes: Routes = [
+  {path: 'login/callback',component: OktaCallbackComponent},
+  {path: 'login',component: LoginComponent},
   {path: 'checkout', component: CheckoutComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'search/:keyword', component: OfferListComponent},
@@ -49,9 +62,10 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OktaAuthModule
   ],
-  providers: [OfferService],
+  providers: [OfferService , {provide: OKTA_CONFIG, useValue:oktaConfig}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

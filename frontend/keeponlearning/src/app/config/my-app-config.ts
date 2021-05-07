654756fb86c462d6d1d95ba293d6ec7a52ed5587
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2021-05-07 20:49:42 +0300

    set up app configuration for OpenID connect

--- frontend/keeponlearning/src/app/config/my-app-config.ts
+++ frontend/keeponlearning/src/app/config/my-app-config.ts
@@ -1,2 +1,8 @@
-export interface MyAppConfig {
+export default {
+  oidc:{
+    client:'0oapf5e8lVnKgsg825d6',
+    issuer:'https://dev-32860632.okta.com/oauth2/default',
+    redirectUri:'http://localhost:4200/login/callback',
+    scopes:['openid','profile','email']
+  }
 }
